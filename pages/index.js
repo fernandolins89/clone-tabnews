6b064aadcb8f59769826a536ec8c42b5e77f815f
fernandolins89 <59851189+fernandolins89@users.noreply.github.com>
2025-01-02 19:39:37 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home () {
 
-    return <h1>Amor, te amo! Se você também me ama, me dá um abraço!</h1>
+    return <h1>Amor, te amo! Se você também me ama, me dá um abraço bem forte!</h1>
 }
 
 export default Home
