d89b17ab2140343b0a1936b55fd36488edef0526
fernandolins89 <59851189+fernandolins89@users.noreply.github.com>
2025-01-10 22:33:21 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone da TabNews para estudos do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-function Home () {
-
-    return <h1>Amor, te amo! Se você também me ama, me dá um abraço bem forte!</h1>
+function Home() {
+  return (
+    <h1>Amor, te amo! Se você também me ama, me dá um abraço bem forte!</h1>
+  );
 }
 
-export default Home
+export default Home;
