72dacff8aea9c8b8e3be35e1454b62ea68b7ba62
fernandolins89 <59851189+fernandolins89@users.noreply.github.com>
2025-03-13 23:09:21 +0000

    add `database,js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,16 @@
-function status(request, response) {
-  response.status(200).json({ chave: "valor" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  try {
+    const result = await database.query("SELECT 1 + 1 as sum;");
+    console.log("Database query result:", result.rows);
+
+    response.status(200).json({ chave: "valor", sum: result.rows[0].sum });
+  } catch (error) {
+    console.error("Erro ao acessar o banco de dados:", error);
+
+    response.status(500).json({ error: "Erro interno no servidor" });
+  }
 }
 
 export default status;
