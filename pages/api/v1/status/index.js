81c6d80ee6d70c68c99f571ed8340e72a4b7227a
fernandolins89 <59851189+fernandolins89@users.noreply.github.com>
2025-03-25 20:13:14 +0000

    add `jsconfig,json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  try {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log("Database query result:", result.rows);
-
-    response.status(200).json({ chave: "valor", sum: result.rows[0].sum });
-  } catch (error) {
-    console.error("Erro ao acessar o banco de dados:", error);
-
-    response.status(500).json({ error: "Erro interno no servidor" });
-  }
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "são acima da média" });
 }
 
 export default status;
